04283d3ae651dda87d4f42ede121db65c9d6f059
Lukasz Szramka <lukasz_szramka@wp.pl>
2022-10-24 21:20:33 +0200

    I added js to the code and published hydrapp v.1.0.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,43 @@
 import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import {
+  registerSW
+} from './pwa.js';
+registerSW();
 
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const glassCounter = document.querySelector('.glass__counter--js');
+const buttonAdd = document.querySelector('.button-add--js');
+const buttonRemove = document.querySelector('.button-remove--js');
+const key = new Date().toLocaleString().slice(0, 10);
+
+let currentGlassCounter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+  currentGlassCounter = localStorageValue;
+} else {
+  localStorage.setItem(key, 0);
+}
+
+glassCounter.innerHTML = currentGlassCounter;
+
+buttonAdd.addEventListener('click', () => {
+  currentGlassCounter++;
+  glassCounter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key, currentGlassCounter);
+
+});
+
+buttonRemove.addEventListener('click', () => {
+  if (currentGlassCounter > 0) {
+    currentGlassCounter--;
+  }
+  glassCounter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key, currentGlassCounter);
+});
